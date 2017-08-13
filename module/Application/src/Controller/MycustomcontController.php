60d8ffafb9dda6769e0495dbcbec15b32c7f3fbc
evilReiko <evilreiko@gmail.com>
2017-08-13 09:04:02 +0300

    how to get another view's html with the layout

--- module/Application/src/Controller/MycustomcontController.php
+++ module/Application/src/Controller/MycustomcontController.php
@@ -49,18 +49,33 @@ class MycustomcontController extends AbstractActionController
 //        readme($currentRowArr['id']);
 //        exit;
         
+        
+        
         // using the renderer to fetch a view's html in the controller's action
+        // WITHOUT layout
 //        $secondView = $this->mycustomactAction();
+//        //$secondView = $this->mycustomactAction(['myvar'=>'myval']);// my technique: (optionally) we can pass associative array of parameters in the action, but you must add that parameter in the action's function
 //        $secondView->setTemplate('application/mycustomcont/mycustomact');// you can set the template here or in the action itself before returning the viewmodel
-//        $html = $this->renderer->render($secondView);
+//        $html = $this->renderer->render($secondView/*, $arr*/);// returns the html of the view without the layout. zend technique: (optionally) we can pass 2nd parameter as associative array of variables for the action
 //        readme($html);exit;
+        
+        // WITH layout
+        $secondView = $this->mycustomactAction();
+        $secondView->setTemplate('application/mycustomcont/mycustomact');// you can set the template here or in the action itself before returning the viewmodel
+        $viewHtml = $this->renderer->render($secondView/*, $arr*/);// returns just the html of the view (without the layout yet)
+        $fullHtml = $this->renderer->render('layout/layout', [
+            'content' => $viewHtml,
+        ]);// returns just the html of the layout (we pass the content of the layout as a variable to the layout! i guess we can pass more parameters of the layout here if we want)
+        readme($fullHtml);exit;
+        
+        
         
         return new ViewModel();
     }
     
-    public function mycustomactAction()
+    public function mycustomactAction(/*$arr = null*/)// we're adding this so the action accepts our custom parameters we pass in the first action
     {
-        $viewModel = new ViewModel();
+        $viewModel = new ViewModel(/*$arr*/);// $arr here is optionally we're adding it
         $viewModel->setTemplate('application/mycustomcont/mycustomact');// here, it's optional to set the template or not. but if fetched from another action, you must set the template of the viewmodel here before returning it or there after getting it
         return $viewModel;
         //return new ViewModel();
